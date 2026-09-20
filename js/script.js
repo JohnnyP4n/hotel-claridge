553133b2d461e5000473bdab7ca84dbd76fbae62
Johnny Pan <panjohnny@live.nl>
2026-09-20 19:18:58 +0200

    refactor: remove unused scroll and submit functions from script.js

--- js/script.js
+++
@@ -1,8 +0,0 @@
-function scrollToContact() {
-    document.getElementById("contact").scrollIntoView({ behavior: "smooth" });
-  }
-  
-  function handleSubmit(event) {
-    event.preventDefault();
-    alert("Bedankt voor je bericht! We nemen zo snel mogelijk contact met je op.");
-  }
